9b4cbf889cdd7fc780f815d72fb23951ba5b3fa7
michaelboegner <cmikeb@gmail.com>
2023-10-21 14:23:23 -0400

    GETSLackMessage passes TestGETSlackMessage() and gets messages from test link mocking slack repsonse

--- slack-service/slack_service.go
+++ slack-service/slack_service.go
@@ -1,5 +1,16 @@
 package slack_service
 
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
 func GETSlackMessages(name string) (message string) {
-	return "Michael is doing a great job. Attaboy!"
+	requestURL := fmt.Sprint("https://webhook.site/56c3dcfb-1056-42e2-abf3-ae668cf14be4")
+	res, _ := http.Get(requestURL)
+
+	resBytes, _ := io.ReadAll(res.Body)
+
+	return string(resBytes)
 }
--- slack-service/slack_service_test.go
+++ slack-service/slack_service_test.go
@@ -1,14 +1,16 @@
 package slack_service
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestGETSlackMessages(t *testing.T) {
 	t.Run("should get messages from test link mocking slack", func(t *testing.T) {
 		got := GETSlackMessages("Mike")
-		want := "Michael is doing a great job. Attaboy!"
+		want := "\"Michael is doing a great job. Attaboy!\""
 
 		if got != want {
-			t.Errorf("got %v, wanted %v", got, want)
+			t.Errorf("got %q, wanted %q", got, want)
 		}
 	})
 
